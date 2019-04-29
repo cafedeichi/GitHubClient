7bb0a695b1067389e75ee7adb5574010c400666a
ichi <>
2019-04-29 13:32:40 +0900

    feat: added TODO comment for access token constant.

--- GitHubClient/Model/APIManager/APIConstants.swift
+++ GitHubClient/Model/APIManager/APIConstants.swift
@@ -10,5 +10,5 @@ import Foundation
 
 struct APIConstants {
     static let baseUrl  = "https://api.github.com/users"
-    static let accessToken = "9e61fa205290744d30674c6b01ed7d8ad00f023f"
+    static let accessToken = "9e61fa205290744d30674c6b01ed7d8ad00f023f" // TODO: overwrite this token.
 }
